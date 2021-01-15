d9daa9e20ac7da857499f706aaba8583e7317be0
i-am-hal <Alastar.Quartz@gmail.com>
2021-01-14 20:34:44 -0900

    Created makefile, favorite game program.

---
+++ hw0/favoriteGame.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/* Print out a description of one of my favorite games of all
+time, A Mind Forever Voyaging, and I give my thoughts on it. */
+int main () {
+    cout << "One of my all time favorite games is a game called 'A Mind Forever Voyaging.'" << endl;
+    cout << "It was a game released by Infocom in 1985, written by Steve Meretzky, one of the" << endl;
+    cout << "notable people who worked there. The game takes place in 2031, where:" << endl;
+    cout << "  \"The world is on the brink of collapse..\"" << endl << endl;
+    
+    cout << "You play as PRISM, the first sentient artificial intelligence, tasked with the mission" << endl;
+    cout << "of entering simulations of the future in which a new political plan, made by some leaders," << endl;
+    cout << "The Plan For Renewed National Purpose, goes into action." << endl;
+    cout << "  \"Will the Plan ensure peace, and prosperity? Or will it set the earth on a suicide" << endl;
+    cout << "  course to destruction? ... Only you can visit places that have never been seen before." << endl;
+    cout << "  Only you can view the future. And only you can know what must be done to save humanity.\"" << endl << endl;
+
+    cout << "In the game, you go further and further into the future, seeing how your small home town" << endl;
+    cout << "changes and falls apart throughout the decades. The game is, in all honesty, very disturbing." << endl;
+    cout << "And, of course, the game is quite political in nature, but nonetheless I belive that this" << endl;
+    cout << "game has been the most relevant than it ever has been, in its ~30 years of life. I have" << endl;
+    cout << "scenes in this game burned into my mind, and believe that I will remember it for a long time." << endl;
+    cout << "I hope to play this game again soon." << endl;
+
+    return 0;
+}
