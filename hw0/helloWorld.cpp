80de6a6b76dd51cb764aeb10e4bd0b7253af8c82
i-am-hal <Alastar.Quartz@gmail.com>
2021-01-14 19:40:35 -0900

    Created 'Hello World' program for HW0.

---
+++ hw0/helloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    //Print out 'Hello, World!' five times.
+    for (int i = 0; i < 5; i++)
+        cout << "Hello, World!" << endl;
+    
+    return 0;
+}
