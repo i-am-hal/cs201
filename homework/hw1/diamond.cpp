80b08a1344ee3a5891bd75fc034db06e024b417e
i-am-hal <Alastar.Quartz@gmail.com>
2021-01-25 14:26:18 -0900

    Finished diamond.cpp, but w/o line oriented input

--- homework/hw1/diamond.cpp
+++ homework/hw1/diamond.cpp
@@ -4,12 +4,14 @@
  * CS 201
 */
 #include <iostream>
+#include <cstdlib>
 using std::cin;
 using std::endl;
 using std::cout;
+using std::abs;
 
 int main () {
-    int diamondWidth = 0; //Width of diamond, input by user.
+    int diamondWidth = 0; //Width of diamond, given by user.
 
     //Continuously prompt user for input until we get an integer > 0
     while (diamondWidth <= 0) {
@@ -24,4 +26,31 @@ int main () {
     //Calculate the size of the rows, and columns (in characters)
     int charSize = 2 * diamondWidth - 1;
 
+    int line = 0;
+    int column = 1;
+    int numSpaces; //The number of spaces needed on either side of hashes
+    int hashMaxima = charSize / 2; //Number at which we've hit maximum number of hashes
+
+    //Print out each line of the diamond..
+    while (line <= charSize) {
+        numSpaces = abs(hashMaxima - line); 
+
+        //Print each column, changing based on how wide it should be
+        while (column <= charSize) {
+            //If column is between the pair of 'space' buffers, print hashes
+            if (column > numSpaces && column <= charSize-numSpaces) {
+                cout << "#";
+            
+            //Otherwise, print out spaces for the buffers
+            } else {
+                cout << " ";
+            }
+            column++;
+        }
+        line++;
+        column = 1;
+        cout << endl;
+    }
+
+    return 0;
 }
