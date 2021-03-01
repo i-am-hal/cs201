ec9259044da21626510bb68bf7c5e95f062788f9
i-am-hal <Alastar.Quartz@gmail.com>
2021-03-01 09:40:40 -0900

    tokenizer.hpp: ReadLine function created

--- homework/hw4/tokenizer.hpp
+++ homework/hw4/tokenizer.hpp
@@ -1,4 +1,16 @@
 /* Alastar Slater
  * March 1, 2021
  * tokenizer.hpp */
+#include <iostream>
+#include <string>
+using std::string;
+using std::getline;
+using std::endl;
+using std::cout;
+using std::cin;
 
+//Will get a line from the user, store it into a variable, and
+// return true if input isn't just spaces, false if otherwise.
+bool ReadLine(string &str) {
+    return false;
+}
