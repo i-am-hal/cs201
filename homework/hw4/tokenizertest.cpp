b77985f01c7883ef6bc3ab6d4c7cd15e68e26029
i-am-hal <Alastar.Quartz@gmail.com>
2021-03-01 23:02:04 -0900

    tokenizertest.cpp:Correctly tests StringToTokensWS

--- homework/hw4/tokenizertest.cpp
+++ homework/hw4/tokenizertest.cpp
@@ -9,8 +9,17 @@
 const vector<string> stringToTokensTests = {
     "print \"Hello!\"",
     "FOR I = 1 TO 5 DO X = X * I END",
-    "begin \"Wow\" end",
+    "begin \"Wow\" End",
     "program testing x = 42 print x end"
+};
+
+//Each of the corresponding resulting token lists 
+// from the various abive inputs to be processed.
+const vector<vector<string>> corrTokenTests = {
+    {"print", "\"Hello!\""},
+    {"FOR", "I", "=", "1", "TO", "5", "DO", "X", "=", "X", "*", "I", "END"},
+    {"begin", "\"Wow\"", "End"},
+    {"program", "testing", "x", "=", "42", "print", "x", "end"}
 };
 
 //Given an input test string, return the set of tokens
@@ -19,8 +28,30 @@ vector<string> returnTokensFromString(const string &str) {
     vector<string> tokens;
     StringToTokenWS(str, tokens);
     return tokens;
-}
+};
+
+//
 
 int main() {
+    cout << "TESTING LEXER (StringToTokensWS).." << endl;
+    for (int i = 0; i < stringToTokensTests.size(); i++) {
+        string testString = stringToTokensTests.at(i);         //Get the string to test
+        vector<string> expectedResults = corrTokenTests.at(i); //Get corr expected results
+        vector<string> returnedTokens = returnTokensFromString(testString);
+
+        for (int j = 0; j < expectedResults.size(); j++) {
+            string expectedToken = expectedResults.at(j);
+            string returnedToken = returnedTokens.at(j);
+
+            if (returnedToken != expectedToken) {
+                cout << "Error: test input '" << testString << "' didn't result in expected output";
+                cout << endl << "Where we expected the token '" << expectedToken << "' ";
+                cout << "we instead got '" << returnedToken << "'" << endl;
+                return 1;
+            }
+        }
+    }
+    cout << "All tests passed!" << endl;
+
     return 0;
 }
