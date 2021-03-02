38e202b6a51d74437088c4be530a571ac6b9aa45
i-am-hal <Alastar.Quartz@gmail.com>
2021-03-01 22:40:29 -0900

    tokenizertest.cpp:Made returnTokensFromString

--- homework/hw4/tokenizertest.cpp
+++ homework/hw4/tokenizertest.cpp
@@ -4,6 +4,23 @@
 
 #include "tokenizer.hpp"
 
-int main() {
+//All of the premade test strings to see how well the program
+// converts inputs into a series of tokens in a list.
+const vector<string> stringToTokensTests = {
+    "print \"Hello!\"",
+    "FOR I = 1 TO 5 DO X = X * I END",
+    "begin \"Wow\" end",
+    "program testing x = 42 print x end"
+};
 
+//Given an input test string, return the set of tokens
+// that are given back from the lexer when given this input.
+vector<string> returnTokensFromString(const string &str) {
+    vector<string> tokens;
+    StringToTokenWS(str, tokens);
+    return tokens;
+}
+
+int main() {
+    return 0;
 }
