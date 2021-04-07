e2fd4992bd68411a3c0aafc17cadac082c12608d
i-am-hal <Alastar.Quartz@gmail.com>
2021-04-07 00:38:02 -0800

    Created lexer.cpp, start of Parser

---
+++ TokiPonaParser/lexer.cpp
@@ -0,0 +1,8 @@
+/* Alastar Slater
+ * April 7, 2021
+ * CS201
+ * lexer.cpp
+ * Describes the lexer which will be used
+ * in the Toki Pona parser. Will have the
+ * defintiions for a Token (word), and also
+ * have a routine to identify parts to speech. */
