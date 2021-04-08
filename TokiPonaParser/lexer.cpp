022b7db588efb3a28c0bc6208e383562595ff411
i-am-hal <Alastar.Quartz@gmail.com>
2021-04-07 22:25:50 -0800

    Finished nextToken, made getWord

--- TokiPonaParser/lexer.cpp
+++ TokiPonaParser/lexer.cpp
@@ -6,7 +6,9 @@
  * in the Toki Pona parser. Will have the
  * defintiions for a Token (word), and also
  * have a routine to identify parts to speech. */
+#include <algorithm>
 #include <string>
+using std::transform;
 using std::string;
 
 //Token types, end of line, verb, preposition, modifer, 
@@ -21,6 +23,7 @@ enum TokenType {
     Eol, 
     Unknown,
     Period,
+    Comma,
     Question,
     Exclaim, 
     Verb, Prep, 
@@ -39,6 +42,14 @@ struct Token {
 //Creates a new instance of a token
 Token newToken(TokenType type, string value) {
     return Token {type, value};
+}
+
+//Takes a string, makes it all lowercase
+string toLower(string str) {
+    for(int i=0; i<str.size(); i++)
+        str[i] = tolower(str.at(i));
+    
+    return str;
 }
 
 //Defines the lexer, which will go through some
@@ -60,6 +71,63 @@ class Lexer {
                 currChar = text.at(pos);
             }
         }
+
+        //Skips over all whitespace characters
+        void skipWhitespace() {
+            //While char is whitespace, skip over it
+            while (isspace(currChar))
+                advance();
+        }
+
+        //Returns true if this character is a part of punctuation or not
+        bool isPunctuation(char chr) {
+            return chr == '.' || chr == ',' || chr == '?' || chr == '!';
+        }
+
+        //Gets a bunch of characters that are seperated by
+        // whitespace or punctuation
+        Token getWord() {
+            string value = "";
+
+            //Get all of the characters while the current character
+            // is neither whitespace, nor punctuation marks
+            while (!isspace(currChar) && !isPunctuation(currChar)) {
+                value.push_back(currChar);
+                advance();
+            }
+
+            //Make the string all lowercase
+            string lowerVal = toLower(value);
+
+            /* CHECK FOR A FEW IMPORTANT PARTICLE WORDS */
+
+            //Catch if this is the direct object marker
+            if (lowerVal == "e")
+                return newToken(E, value);
+            
+            //Catch if this is the command / addressing
+            else if (lowerVal == "o")
+                return newToken(O, value);
+            
+            //Catch if this is the 'of' partcile
+            else if (lowerVal == "pi")
+                return newToken(Pi, value);
+            
+            //Catch if this is the main verb marker
+            else if (lowerVal == "li")
+                return newToken(Li, value);
+            
+            //Catch if this is the sentence connector
+            else if (lowerVal == "la")
+                return newToken(La, value);
+            
+            //If this is one of the pronouns, mark this premtively as a pronoun
+            else if (lowerVal == "mi" || lowerVal == "ona" || lowerVal == "sina" || lowerVal == "ni")
+                return newToken(Pronoun, lowerVal);
+            
+            else //Otherwise, show it as unknown for now
+                return newToken(Unknown, value);
+        }
     
     public:
         //Creates the lexer, only needs the text which should be fed through
@@ -78,6 +146,41 @@ class Lexer {
         }
 
         //Lexer will attempt to get a new token to return.
-        Token nextToken() {}
+        Token nextToken() {
+            //Continue trying to get a token while we can
+            while (pos < text.size() && !error) {
+                //If this is whitespace, skip over all whitespace
+                if (isspace(currChar))
+                    skipWhitespace();
+                
+                //Return period token
+                else if (currChar == '.') {
+                    advance();
+                    return newToken(Period, ".");
+
+                //Skip over comma
+                } else if (currChar == ',') {
+                    advance();
+                    //return newToken(Comma, ",");
+
+                //Return exclaimation mark token
+                } else if (currChar == '!') {
+                    advance();
+                    return newToken(Exclaim, "!");
+
+                //Return question mark token
+                } else if (currChar == '?') {
+                    advance();
+                    return newToken(Question, "?");
+
+                //Otherwise, try to get a word token
+                } else {
+                    return getWord();
+                }
+            }
+
+            //Outside of bounds, or error, return Eol
+            return newToken(Eol, "");
+        }
 };
 
